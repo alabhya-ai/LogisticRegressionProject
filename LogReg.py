16fde99d5c0b2b18abd311b4b28c9f28bb888738
alabhya-ai <63790847+alabhya-ai@users.noreply.github.com>
2022-05-28 19:52:07 +0545

    .

--- LogReg.py
+++ LogReg.py
@@ -17,10 +17,12 @@ class LogisticRegression(object):
         independent_var: Data containing features (independent variable)
         dependent_var: Data containing labels (dependent variable)
     """
-    self.m =  dependent_var.shape[0] # no. of data points
-    self.y = dependent_var # dependent variable
-    self.X = (independent_var - independent_var.mean()) / independent_var.std() # standardization
+    self.x = independent_var # training data (X)
+    self.m =  dependent_var.size # no. of data points
+    self.y = dependent_var.reshape(self.m, 1) # dependent variable
+    self.X = (independent_var - independent_var.mean(axis=0)) / independent_var.std(axis=0) # standardization
     self.X = np.hstack((np.ones((self.m, 1)), self.X)) # adding column of ones in independent variable
+    self.n = self.X.shape[1] # no. of features
     self.theta = np.zeros((self.X.shape[1], 1)) # setting initial theta as zeros
 
   def gd(self, num_of_iterations, alpha, hist):
@@ -29,8 +31,8 @@ class LogisticRegression(object):
     if hist:
       l = np.ones((1, num_of_iterations)) # to store loss history
       for i in range(num_of_iterations):
-        h = 1.0 / (1 + np.exp(-np.dot(self.X, self.theta).astype(np.float))) # hypothesis (sigmoid)
-        grad = np.dot(self.X.T, np.array([h[i] - self.y[i] for i in range(self.m)])) / self.m # gradient
+        h = 1.0 / (1 + np.exp(-np.dot(self.X, self.theta).astype(np.float64))) # hypothesis (sigmoid)
+        grad = np.dot(self.X.T, h - self.y) / self.m # gradient
         self.theta = self.theta - (alpha * grad) # theta update rule
         l[0, i] = np.dot(self.y.T, np.log(h)) + np.dot((1 - self.y).T, np.log(1 - h)) # loss
 
@@ -38,8 +40,8 @@ class LogisticRegression(object):
 
     else:
       for i in range(num_of_iterations):
-        h = 1.0 / (1 + np.exp(-np.dot(self.X, self.theta).astype(np.float))) # hypothesis (sigmoid)
-        self.theta = self.theta - (alpha * np.dot(self.X.T, np.array([h[i] - self.y[i] for i in range(self.m)])) / self.m) # theta update rule
+        h = 1.0 / (1 + np.exp(-np.dot(self.X, self.theta).astype(np.float64))) # hypothesis (sigmoid)
+        self.theta = self.theta - (alpha * np.dot(self.X.T, h - self.y) / self.m) # theta update rule
 
   def sgd(self, num_of_iterations, alpha, hist):
     '''runs stochastic gradient descent and returns best theta'''
@@ -49,35 +51,37 @@ class LogisticRegression(object):
 
       for i in range(num_of_iterations):
         r = np.random.randint(0, self.m)
-        Xr, Yr = self.X[r, :], self.y[r] # random data point
-        Hr = 1.0 / (1 + np.exp(-np.dot(Xr, self.theta).astype(np.float))) # hypothesis given by the random data point (sigmoid)
-        self.theta = self.theta - ((alpha * (Xr.T * (Hr - Yr))) / self.m) # theta update rule
-        l[0, i] = (Yr * np.log(Hr)) + ((1 - Yr) * np.log(1 - Hr)) # loss
+        Xr, Yr = self.X[r, :].reshape((1, self.n)), self.y[r] # random data point
+        Hr = 1.0 / (1 + np.exp(-np.dot(Xr, self.theta).astype(np.float64))) # hypothesis given by the random data point (sigmoid)
+        self.theta = self.theta - (alpha * (Xr.T * (Hr[0] - Yr)) / self.m) # theta update rule
+        l[0, i] = (Yr * np.log(Hr[0])) + ((1 - Yr) * np.log(1 - Hr[0])) # loss
 
       return -l
 
     else:
       for i in range(num_of_iterations):
         r = np.random.randint(0, self.m)
-        Xr, Yr = self.X[r, :], self.y[r] # random data point
-        Hr = 1.0 / (1 + np.exp(-np.dot(Xr, self.theta).astype(np.float))) # hypothesis given by the random data point (sigmoid)
-        self.theta = self.theta - ((alpha * (Xr.T * (Hr - Yr))) / self.m) # theta update rule
+        Xr, Yr = self.X[r, :].reshape((1, self.n)), self.y[r] # random data point
+        Hr = 1.0 / (1 + np.exp(-np.dot(Xr, self.theta).astype(np.float64))) # hypothesis given by the random data point (sigmoid)
+        self.theta = self.theta - (alpha * (Xr.T * (Hr[0] - Yr)) / self.m) # theta update rule
+
+  def prep(self, X):
+    '''returns value to be passed to hypothesis function (for the prediction methods)'''
+    X_ = np.vstack((self.x, X))
+    X_ = (X_ - X_.mean(axis=0)) / X_.std(axis=0) # standardization
+    X = X_[self.m:, :] # given X
+    return self.theta[0, 0] + np.dot(X, self.theta[1:]) # value to be passed to sigmoid
 
   def pred(self, X, threshold):
     '''returns predictions made by the model according to the given threshold on given X'''
-    X = (X - X.mean()) / X.std()# standardization
-    z = self.theta[0, 0] + np.dot(X, self.theta[1:]) # value to be passed to sigmoid
-    h = 1.0 / (1 + np.exp(-z.astype(np.float))) # hypothesis
 
-    return np.array([1 if i >= threshold else 0 for i in h])
+    h = 1.0 / (1 + np.exp(-self.prep(X).astype(np.float64))) # hypothesis
+    return [1 if i >= threshold else 0 for i in h]
 
   def pred_prob(self, X):
     '''Returns the probabilities output by the model on given X.'''
 
-    X = (X - X.mean()) / X.std()
-    z = self.theta[0, 0] + np.dot(X, self.theta[1:]) # value to be passed to sigmoid
-
-    return 1.0 / (1 + np.exp(-z.astype(np.float)))
+    return 1.0 / (1 + np.exp(-self.prep(X).astype(np.float64)))
 
   def regularize(self, lambda_, set_param=True):
     '''returns regularized version of theta found by optimization algorithm.
@@ -90,13 +94,11 @@ class LogisticRegression(object):
     if set_param:
       self.theta[1:, 0] = self.theta[1:, 0] * lambda_ / self.m
     else:
-      tmp = np.zeros(self.theta.shape)
-      tmp[0, 0] = self.theta[0, 0] # no update required for constant term
-      tmp[1:, 0] = self.theta[1:, 0] * lambda_ / self.m
+      tmp = self.theta.copy()[1:, 0] * lambda_ / self.m # no update required for constant term
       return tmp
 
   def loss(self):
-    h = 1.0 / (1 + np.exp(-np.dot(self.X, self.theta).astype(np.float))) # hypothesis (sigmoid)
+    h = 1.0 / (1 + np.exp(-np.dot(self.X, self.theta).astype(np.float64))) # hypothesis (sigmoid)
     return (np.dot(self.y.T, np.log(h)) + np.dot((1 - self.y).T, np.log(1 - h))) / -self.m
 
   def score(self, threshold):
@@ -105,7 +107,7 @@ class LogisticRegression(object):
     threshold: threshold for predicting whether data point lies in class 0 or 1
     '''
 
-    s = 1.0 / (1 + np.exp(-np.dot(self.X, self.theta).astype(np.float))) # probabilities
+    s = 1.0 / (1 + np.exp(-np.dot(self.X, self.theta).astype(np.float64))) # probabilities
     p = np.array([1 if i >= threshold else 0 for i in s]) # predictions
     tp, tn, fp, fn = 0, 0, 0, 0 # true positive, true negative, false positive, false negative
 
@@ -121,7 +123,7 @@ class LogisticRegression(object):
         else:
           fn += 1
 
-    if not (tp == 0 or fp == 0 or fn == 0):
+    if tp and fp and fn:
       p = tp / (tp + fp) # precision score
       r = tp / (tp + fn) # recall score
       
@@ -153,4 +155,4 @@ class LogisticRegression(object):
       if cost:
         return self.gd(num_of_iterations=ret[1], alpha=ret[2], hist=history), self.loss()
       else:
-        self.gd(num_of_iterations=ret[1], alpha=ret[2], hist=history)
+        return self.gd(num_of_iterations=ret[1], alpha=ret[2], hist=history)
